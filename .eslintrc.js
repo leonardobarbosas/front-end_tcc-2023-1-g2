9602425d4d8e193c553ebb706f2d7aae03040eb4
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-02-17 11:18:51 -0300

    started to do the register page

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/react-in-jsx-scope': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styled';
+import Form from '../../components/Form';
+
+export default function Register() {
+  return (
+    <>
+      <Title>Olá</Title>
+      <Form />
+    </>
+  );
+}
