928b6c9577002e22ee2b002ed3e790c9e469ddd4
Arthur Piloto <arthurpiloto.github@gmail.com>
2023-02-19 19:51:44 -0300

    uploading files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from './styles/GlobalStyles';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
