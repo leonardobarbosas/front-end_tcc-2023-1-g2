eacbf6768522e7255a3e9dac8cc4af8e10ada0b4
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2023-02-24 10:13:24 -0300

    front-end of the first sprint done

---
+++ assets/js/choose_page.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const goBackButton = document.getElementById('go_back_button')
+goBackButton.addEventListener('click', () => {
+    const url = 'http://127.0.0.1:5500/pages/register_page.html'
+    document.location.href = url
+})
+
+const userButton = document.getElementById('usuario')
+userButton.addEventListener('click', () => {
+    const url = 'http://127.0.0.1:5500/index.html'
+    document.location.href = url
+})
