af0ef299351eb358f2f9bef51ba7bc05eac987b5
Arthur Augusto <98403987+arthuraugusn@users.noreply.github.com>
2023-03-16 09:24:56 -0300

    arrumando lógica

--- firebase/firebaseGoogleLoginRegister.js
+++ firebase/firebaseGoogleLoginRegister.js
@@ -1,38 +1,29 @@
 import {app} from "./firebaseApp.js"
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js"
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js"
+//https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js
+//firebase/auth
 
 const provider = new GoogleAuthProvider()
 
 const auth = getAuth(app)
 
-const result = await signInWithPopup(auth, provider)
+const getInfosUser = async()=>{
+    const result = await signInWithRedirect(auth, provider)
+    //const credential = GoogleAuthProvider.credentialFromResult(result)
 
-const credential = GoogleAuthProvider.credentialFromResult(result)
+    const user = result.user
 
-const token = credential.accessToken
+    const userInfos = {
+        email: user.email,
+        nome: user.displayName,
+        uid: user.uid
+    }
 
-const user = result.user
-
-const getName = async()=>{
-    const name = user.displayName
-
-    return name
-}
-
-const getEmail = async()=>{
-    const email = user.email
-
-    return email
-}
-
-const getUidPassword = async()=>{
-    const uid = user.uid
-
-    return uid
+    return userInfos
 }
 
 
 
 export{
-    getEmail,getName,getUidPassword
+    getInfosUser
 }
