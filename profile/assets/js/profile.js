f331295d2172b6739aa551c09e282bc390fe0a67
Arthur Augusto <98403987+arthuraugusn@users.noreply.github.com>
2023-03-17 11:04:33 -0300

    resolvendo erros

--- profile/assets/js/profile.js
+++ profile/assets/js/profile.js
@@ -1,15 +1,19 @@
 'use strict'
 
-import {editPerfil, perfilNormal} from "./functions/functionsProfile.js"
+import { editPerfil, perfilNormal } from "./functions/functionsProfile.js"
 
 const buttonEdit = document.querySelector('.button_edit')
 
-buttonEdit.addEventListener('click', ()=>{
-    editPerfil()
-    const buttonReturn = document.querySelector('.button_return')
+buttonEdit.addEventListener('click', () => {
+    if (!document.body.contains(document.querySelector('.button_save'))) {
+        editPerfil()
+        const buttonReturn = document.querySelector('.button_return')
 
-buttonReturn.addEventListener('click', ()=>{
-    perfilNormal()
-})
+        buttonReturn.addEventListener('click', () => {
+            perfilNormal()
+
+
+        })
+    }
 })
 
